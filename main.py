d8fd2c28dd7ebd22e46be9406fd5206a831c2480
cameronlester <pickingemailsarehard@gmail.com>
2021-08-02 00:17:24 +0000

    Added if __name__ function

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 # to calculate barcode's 13th digit error.
 import random
-# make random 12 digits
-bar = [9,3,1,1,5,9,9,4,1,6,4,6] # headgear check 4
-# make random 13 digits
-bar2 = []
-for _ in range(12):
-   bar2.append(random.randint(0,9))
-print(bar, bar2)
+
+
 def bar_check(bar):
     line1 = sum(bar[::2])
     line2 = sum(bar[1::2])
@@ -17,5 +12,12 @@ def bar_valid(bar_code):
   check = bar_check(bar_code)
   return check == bar_code[-1]
 
-print (bar_check(bar)) 
-print (bar_valid(bar2))
+if __name__ == "__main__":
+  bar = [9,3,1,1,5,9,9,4,1,6,4,6] # headgear check 4
+  # make random 13 digits
+  bar2 = []
+  for _ in range(12):
+   bar2.append(random.randint(0,9))
+  print(bar, bar2) 
+  print (bar_check(bar)) 
+  print (bar_valid(bar2))
